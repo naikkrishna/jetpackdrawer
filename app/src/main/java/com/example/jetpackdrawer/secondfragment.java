f2c50a7ab394712f08df155f4670b35c616749c9
naikkrishna <knaik9669@gmail.com>
2019-11-30 14:05:28 -0500

    jet

---
+++ app/src/main/java/com/example/jetpackdrawer/secondfragment.java
@@ -0,0 +1,31 @@
+package com.example.jetpackdrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class secondfragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_secondfragment, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+}
